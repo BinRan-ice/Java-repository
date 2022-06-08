f079e086ba77dfa325c80849753fd038bc100896
BinRan-ice <lonelinessll@icloud.com>
2022-06-08 12:51:54 +0800

    day32-code

---
+++ day32-code/src/cn/itcast/day32/demo01/Servlet/Demo01Servlet.java
@@ -0,0 +1,35 @@
+package cn.itcast.day32.demo01.Servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/*
+    Servlet快速入门
+ */
+public class Demo01Servlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    //提供服务的方法
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("hello servlet");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
